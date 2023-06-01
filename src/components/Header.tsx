5da10b3fd3397ee4f9c74a6a5accf0f46d6287a8
마조리카 <47102222+neu5563@users.noreply.github.com>
2023-06-01 15:38:39 +0900

    fix: 헤더 스타일 수정 및 로고 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,104 +1,93 @@
 import tw from 'tailwind-styled-components'
 import { Link, useLocation } from 'react-router-dom'
-import { actions } from '@/store/my'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '@/auth'
+import { actions } from '@/store/my'
+import logo from '@/assets/images/logo.png'
 
-const StyleHeader = tw.header`
-
+const StyledHeader = tw.header`
   flex
   justify-between
+  flex-wrap
   items-center
-  px-[80px]
-  py-10
+  px-5
+  py-7
   w-full
   text-white
   font-bold
   text-xl
+  break-keep
   mobile:px-5
   mobile:text-base
-  break-keep
-  flex-wrap
-  
 `
 
 const StyledMenu = tw.div`
   flex
   gap-2
   font-normal
-  cursor-pointer
+  items-center
 `
 
-const header = () => {
+const StyledLink = tw(Link)`
+  hover:underline
+  underline-offset-4
+`
+
+const Header = () => {
   const location = useLocation()
   const dispatch = useDispatch()
 
   const myInfo = useSelector((state: any) => state.my.myInfo)
 
-  console.log(myInfo)
+  const isLinkActive = (path: string) => location.pathname === path
+  const getLinkClassName = (path: string) =>
+    isLinkActive(path) ? 'underline' : ''
+
+  const handleLogout = () => {
+    logout()
+    dispatch(actions.logout())
+  }
 
   return (
-    <StyleHeader>
-      <Link to="/" className="cursor-pointer">
+    <StyledHeader>
+      <Link to="/" className="flex cursor-pointer items-center gap-2">
+        <img src={logo} alt="logo" className="h-10 w-auto mobile:h-5" />
         마조리카 블로그
       </Link>
 
-      {myInfo?.email ? (
-        <StyledMenu>
-          <Link
-            to="/"
-            onClick={() => {
-              logout()
-              dispatch(actions.logout())
-            }}
-            className={`hover:underline
-          ${location.pathname === '/logout' ? 'underline' : ''}
-          `}
-          >
-            로그아웃
-          </Link>
+      <StyledMenu>
+        {myInfo?.email ? (
+          <>
+            <StyledLink
+              to="/"
+              onClick={handleLogout}
+              className={getLinkClassName('/logout')}
+            >
+              로그아웃
+            </StyledLink>
 
-          <Link
-            to="/post"
-            className={`hover:underline
-        ${location.pathname === '/post' ? 'underline' : ''}
-        `}
-          >
-            게시판
-          </Link>
-          <Link
-            to="/gpt"
-            className={`hover:underline
-        ${location.pathname === '/gpt' ? 'underline' : ''}
-        `}
-          >
-            Gpt글쓰기
-          </Link>
-        </StyledMenu>
-      ) : (
-        <StyledMenu>
-          {' '}
-          <Link
-            to="/login"
-            //location.pathname === '/login' ? 'underline' : ''
-            className={`hover:underline
-        ${location.pathname === '/login' ? 'underline' : ''}
-        `}
-          >
-            로그인
-          </Link>
-          <Link
-            to="/signup"
-            className={`hover:underline
-        ${location.pathname === '/signup' ? 'underline' : ''}
-        `}
-          >
-            회원가입
-          </Link>
-        </StyledMenu>
-      )}
-    </StyleHeader>
+            <StyledLink to="/post" className={getLinkClassName('/post')}>
+              게시판
+            </StyledLink>
+
+            <StyledLink to="/gpt" className={getLinkClassName('/gpt')}>
+              Gpt글쓰기
+            </StyledLink>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className={getLinkClassName('/login')}>
+              로그인
+            </Link>
+
+            <Link to="/signup" className={getLinkClassName('/signup')}>
+              회원가입
+            </Link>
+          </>
+        )}
+      </StyledMenu>
+    </StyledHeader>
   )
 }
 
-export default header
+export default Header
