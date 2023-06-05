7a8f6077005970eeda0c094ae61c95b38795bbd2
마조리카 <47102222+neu5563@users.noreply.github.com>
2023-06-05 16:11:19 +0900

    fix: 메인 페이지 구성 약간 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,35 +15,39 @@ const Main = () => {
   }, [])
 
   return (
-    <div className="w-full  overflow-hidden">
-      <img className="h-screen w-full" src={mainImg} alt="main" />
+    <div className="flex w-full flex-col justify-center gap-5  overflow-hidden">
+      {/* <img className="h-screen w-full" src={mainImg} alt="main" />
+       */}
+      <p className="flex h-[calc(100vh-100px)] w-full items-center justify-center font-bold text-white">
+        스크롤 에니메이션 테스트 중! 아래로 스크롤 해보세요!
+      </p>
       <div
         ref={animatedItem[0].ref}
         style={animatedItem[0].style}
-        className="h-[500px]"
+        className="flex h-screen"
       >
-        <img className="h-[500px] w-auto" src={mainImg} alt="main" />
+        <img className="h-screen w-auto" src={mainImg} alt="main" />
       </div>
       <div
         ref={animatedItem[1].ref}
         style={animatedItem[1].style}
-        className="h-[500px]"
+        className="flex h-screen"
       >
-        <img className="h-[500px] w-auto" src={mainImg} alt="main" />
+        <img className="h-screen w-auto" src={mainImg} alt="main" />
       </div>
       <div
         ref={animatedItem[2].ref}
         style={animatedItem[2].style}
-        className="h-[500px]"
+        className="flex h-screen"
       >
-        <img className="h-[500px] w-auto" src={mainImg} alt="main" />
+        <img className="h-screen w-auto" src={mainImg} alt="main" />
       </div>
       <div
         ref={animatedItem[3].ref}
         style={animatedItem[3].style}
-        className="h-[500px]"
+        className="flex h-screen"
       >
-        <img className="h-[500px] w-auto" src={mainImg} alt="main" />
+        <img className="h-screen w-auto" src={mainImg} alt="main" />
       </div>
     </div>
   )
