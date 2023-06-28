94dd11cc40170d860b8bcaf5feaecab576d00b7d
마조리카 <neu5563@naver.com>
2023-06-28 22:49:23 +0900

    fix: redux getDefaultMiddleware 사용 형태 변경 적용

--- src/store/index.ts
+++ src/store/index.ts
@@ -22,9 +22,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 export const persistor = persistStore(store)
